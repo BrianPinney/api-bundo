5f7942252c6118bde6b890fdcddaa833b1f3dc95
Brian J. Pinney <brianj.pinney@gmail.com>
2023-05-09 20:46:02 -0400

    comment out some code.

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ import cors from "cors"
 import { addDock, deleteDock, getAllDocks, updateDock } from "./src/dockEndPoints.js"
 
 
-const PORT = 3000  
+// const PORT = 3000  
 const app = express()
 
 app.use(cors())
@@ -15,8 +15,8 @@ app.get("/docks", getAllDocks)
 app.patch("/docks/:dockId", updateDock)
 app.delete("/docks/:dockId", deleteDock)
 
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}...`)
-})
+// app.listen(PORT, () => {
+//     console.log(`Listening on port: ${PORT}...`)
+// })
 
 export const api = functions.https.onRequest(app) 
